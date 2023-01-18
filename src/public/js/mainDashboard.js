19f91b907678402a83f9fd713eeabc005741cf9c
20120217 <cauthuminhtri10@gmail.com>
2023-01-19 02:08:26 +0700

    update banner

--- src/public/js/mainDashboard.js
+++ src/public/js/mainDashboard.js
@@ -1,6 +1,7 @@
+const banner = document.querySelector('.banner');
 
 // Swiper init
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".examListSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
@@ -8,3 +9,16 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+// Banner event
+function bannerSlide() {
+    var count = 1;
+    setInterval(() => {
+        if (count <= 5) { ++count; }
+        else { count = 1; }
+        banner.innerHTML = `<img src="image/banner/${count}.png" alt="">`;
+        console.log(count);
+    }, 3000)
+}
+
+bannerSlide();
