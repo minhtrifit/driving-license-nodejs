34ee68c17fa65d24b4c302f6e88e63052ab0a4c0
minhtrifit <minhtri.fit@gmail.com>
2023-02-12 10:14:51 +0700

    update deploy

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,8 +1,13 @@
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = process.env.DB_PORT || 5432;
+const DB_NAME = process.env.DB_NAME || "DRIVING_LICENSE";
+const DB_USER = process.env.DB_USER || "postgres";
+const DB_PASSWORD = process.env.DB_PASSWORD || "123";
+
 module.exports = {
-    host: 'localhost',
-    port: 5432,
-    database: 'DRIVING_LICENSE',
-    user: 'postgres',
-    password: '123',
-    max: 30 // use up to 30 connections
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
 };
